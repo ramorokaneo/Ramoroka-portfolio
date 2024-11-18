5a614bcfbe365c16b80a3aeb476041bbb4dbd44b
Neo Ramoroka <114235812+ramorokaneo@users.noreply.github.com>
2024-11-18 16:30:20 +0200

    first commit

---
+++ script.js
@@ -0,0 +1,5 @@
+document.getElementById('contact-form').addEventListener('submit', (e) => {
+    e.preventDefault();
+    alert('Thank you for reaching out! I will get back to you soon.');
+  });
+  
